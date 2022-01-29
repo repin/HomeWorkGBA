4c8f84f02ace1dd1dba834c74cb86459788f4a3e
repin <sasharepin@Mail.ru>
2022-01-29 17:31:34 +0300

    Добавьте файлы проекта.

---
+++ HomeWorkGBA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeWorkGBA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
